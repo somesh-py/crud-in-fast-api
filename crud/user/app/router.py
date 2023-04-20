6949639e545bd680d6e39e22c8564b22c65db191
somesh-py <someshsharma615@gmail.com>
2023-04-20 23:33:11 +0530

    create login credintial and function in fastapi

--- crud/user/app/router.py
+++ crud/user/app/router.py
@@ -49,30 +49,52 @@ async def read_item(request:Request,Name:str=Form(...),
 
 
 
-@manager.user_loader()
-async def load_user(email:str):
-     if await User.exists(email=email):
-        user=await User.get(email=email)
-        return user
+# @manager.user_loader()
+# async def load_user(email:str):
+#      if await User.exists(email=email):
+#         user=await User.get(email=email)
+#         return user
 
-@router.post('/loginuser/')
-async def login(request:Request,email:str=Form(...),
-                password:str=Form(...)):
-        email=email
-        user= await load_user(email)
-        if not user:
-             return {'USER NOT REGISTERED'}
-        elif not verify_password(password,user.password):
-             return {'PASSWORD IS WRONG'}
+# @router.post('/loginuser/')
+# async def login(request:Request,email:str=Form(...),
+#                 password:str=Form(...)):
+#         email=email
+#         user= await load_user(email)
+#         if not user:
+#              return {'USER NOT REGISTERED'}
+#         elif not verify_password(password,user.password):
+#              return {'PASSWORD IS WRONG'}
         
-        access_token=manager.create_access_token(
-             data=dict(sub=email)
-        )
+#         access_token=manager.create_access_token(
+#              data=dict(sub=email)
+#         )
 
-        if "_messages" not in request.session:
-            request.session['_messages']=[]
-            new_dict={'user_id':str(user.id),"email":email,"access_token":str(access_token)}
-            request.session['_messages'].append(
-                 new_dict
-            )
-        return RedirectResponse("/update/",status_code=status.HTTP_302_FOUND)
+#         if "_messages" not in request.session:
+#             request.session['_messages']=[]
+#             new_dict={'user_id':str(user.id),"email":email,"access_token":str(access_token)}
+#             request.session['_messages'].append(
+#                  new_dict
+#             )
+#         return RedirectResponse("/update/",status_code=status.HTTP_302_FOUND)
+
+@router.post("/logindata/",response_class=HTMLResponse)
+async def login_user(request:Request,Email:str=Form(...),
+                     Password:str=Form(...)):
+     if await User.exists(email=Email):
+          data=await User.get(email=Email)
+          passwword1=data.password
+          temppassword=get_password_hash(Password)
+          
+          def verify_password(plain_password,hashed_password):
+               return pwd_context.verify(plain_password,hashed_password)
+          
+          if verify_password(Password,passwword1):
+               print("password not matched")
+               return templates.TemplateResponse("update.html",{'request':request})
+          else:
+               print("password incorrect")
+               return templates.TemplateResponse("login.html",{'request':request,"messages":"password was incorrect"})
+               
+     else:
+           print("email incorrect")
+           return templates.TemplateResponse("login.html",{'request':request,"messages":"email was incorrect"})
