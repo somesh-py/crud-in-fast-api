08d86aecebb33c6d0127769731c79da02823ca69
somesh-py <someshsharma615@gmail.com>
2023-04-21 21:18:57 +0530

    create delete function and update template and render it.

--- crud/user/app/router.py
+++ crud/user/app/router.py
@@ -1,52 +1,47 @@
-from fastapi import APIRouter,Request,Form,status
+from fastapi import APIRouter, Request, Form, status
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse,RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from .models import User
 import passlib
 from passlib.context import CryptContext
 from fastapi_login import LoginManager
 
 
-router=APIRouter()
+router = APIRouter()
 
-SECRET='your-secret-key'
-manager=LoginManager(SECRET,token_url='/auth/token')
-templates=Jinja2Templates(directory="app/templates")
-pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
+SECRET = 'your-secret-key'
+manager = LoginManager(SECRET, token_url='/auth/token')
+templates = Jinja2Templates(directory="app/templates")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password,hashed_password):
-     return pwd_context.verify(plain_password,hashed_password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
 
 def get_password_hash(password):
-     return pwd_context.hash(password)
+    return pwd_context.hash(password)
 
-@router.get("/update/")
-async def read_item(request:Request):
-     return templates.TemplateResponse("update.html",{'request':request})
 
-@router.get("/login/",response_class=HTMLResponse)
-async def read_item(request:Request):
-     return templates.TemplateResponse("login.html",{'request':request})
+@router.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
 
-@router.get("/",response_class=HTMLResponse)
-async def read_item(request:Request):
-    return templates.TemplateResponse("signup.html",{"request":request})
 
-@router.post("/registration/",response_class=HTMLResponse)
-async def read_item(request:Request,Name:str=Form(...),
-                    Email:str=Form(...),
-                    Phone:str=Form(...),
-                    Password:str=Form(...)):
-    
-        if await User.filter(email=Email).exists():
-            return RedirectResponse("/",status_code=status.HTTP_302_FOUND)
-        
-        elif await User.filter(phone=Phone).exists():
-             return RedirectResponse("/",status_code=status.HTTP_302_FOUND)
-        else:
-            user_obj= await User.create(email=Email,name=Name,phone=Phone,password=get_password_hash(Password))
-            return RedirectResponse("/login/",status_code=status.HTTP_302_FOUND)
+@router.post("/registration/", response_class=HTMLResponse)
+async def read_item(request: Request, Name: str = Form(...),
+                    Email: str = Form(...),
+                    Phone: str = Form(...),
+                    Password: str = Form(...)):
 
+    if await User.filter(email=Email).exists():
+        return RedirectResponse("/", status_code=status.HTTP_302_FOUND)
+
+    elif await User.filter(phone=Phone).exists():
+        return RedirectResponse("/", status_code=status.HTTP_302_FOUND)
+    else:
+        user_obj = await User.create(email=Email, name=Name, phone=Phone, password=get_password_hash(Password))
+        return RedirectResponse("/login/", status_code=status.HTTP_302_FOUND)
 
 
 # @manager.user_loader()
@@ -64,7 +59,7 @@ async def read_item(request:Request,Name:str=Form(...),
 #              return {'USER NOT REGISTERED'}
 #         elif not verify_password(password,user.password):
 #              return {'PASSWORD IS WRONG'}
-        
+
 #         access_token=manager.create_access_token(
 #              data=dict(sub=email)
 #         )
@@ -77,24 +72,56 @@ async def read_item(request:Request,Name:str=Form(...),
 #             )
 #         return RedirectResponse("/update/",status_code=status.HTTP_302_FOUND)
 
-@router.post("/logindata/",response_class=HTMLResponse)
-async def login_user(request:Request,Email:str=Form(...),
-                     Password:str=Form(...)):
-     if await User.exists(email=Email):
-          data=await User.get(email=Email)
-          passwword1=data.password
-          temppassword=get_password_hash(Password)
-          
-          def verify_password(plain_password,hashed_password):
-               return pwd_context.verify(plain_password,hashed_password)
-          
-          if verify_password(Password,passwword1):
-               print("password not matched")
-               return templates.TemplateResponse("update.html",{'request':request})
-          else:
-               print("password incorrect")
-               return templates.TemplateResponse("login.html",{'request':request,"messages":"password was incorrect"})
-               
-     else:
-           print("email incorrect")
-           return templates.TemplateResponse("login.html",{'request':request,"messages":"email was incorrect"})
+@router.get("/login/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("login.html", {'request': request})
+
+
+@router.post("/logindata/", response_class=HTMLResponse)
+async def login_user(request: Request, Email: str = Form(...),
+                     Password: str = Form(...)):
+    if await User.exists(email=Email):
+        data = await User.get(email=Email)
+        passwword1 = data.password
+        temppassword = get_password_hash(Password)
+
+        def verify_password(plain_password, hashed_password):
+            return pwd_context.verify(plain_password, hashed_password)
+
+        if verify_password(Password, passwword1):
+            print("password not matched")
+            return RedirectResponse('/table/')
+        else:
+            print("password incorrect")
+            return templates.TemplateResponse("login.html", {'request': request, "messages": "password was incorrect"})
+
+    else:
+        print("email incorrect")
+        return templates.TemplateResponse("login.html", {'request': request, "messages": "email was incorrect"})
+
+
+@router.get("/table/", response_class=HTMLResponse)
+async def table(request: Request):
+    persons = await User.all()
+    return templates.TemplateResponse("table.html", {'request': request, 'persons': persons})
+
+
+@router.get("/update/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: int):
+    user = await User.get(id=id)
+    return templates.TemplateResponse("update.html", {'request': request, 'id': user})
+
+
+@router.post("/update-details/", response_class=HTMLResponse)
+async def updatedetails(request: Request, Name: str = Form(...),
+                        Email: str = Form(...),
+                        Phone: str = Form(...),
+                        Id: int = Form(...)):
+    user = await User.filter(id=Id).update(name=Name, email=Email, phone=Phone)
+    return RedirectResponse("/table/")
+
+
+@router.get("/delete/{id}", response_class=HTMLResponse)
+async def delete(request: Request, id: int):
+    user = await User.filter(id=id).delete()
+    return RedirectResponse('/table/')
